4109676067b6857fbf4d77a5f1c4c7f3046888b5
AIZAK89 <bacevedolepe@gmail.com>
2023-10-10 01:38:18 -0300

    act y perfil

---
+++ vistas/ActividadView.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+
+const ActividadView = () => {
+    return (
+        <View>
+            <Text
+                style={{
+                    fontSize: 30,
+                    textAlign: "center",
+                    marginTop: "20%"
+                }}
+            >ActividadView</Text>
+        </View>
+    );
+    }
+export default ActividadView;
---
+++ vistas/PerfilView.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+
+const PerfilView = () => {
+    return (
+        <View>
+            <Text
+                style={{
+                    fontSize: 30,
+                    textAlign: "center",
+                    marginTop: "20%"
+                }}
+            >PerfilView</Text>
+        </View>
+    );
+    }
+export default PerfilView;
